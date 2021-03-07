a6ea96f4b047df37587daba2b935e10ba735661e
Jimmy Situ <web@jimmystone.cn>
2021-03-07 15:17:32 +0800

    Add gcov test

---
+++ baremetal/gcov-test/gcov-test.c
@@ -0,0 +1,23 @@
+#include "util.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "gcov-test.h"
+
+void fun1(){
+    int a = 1;
+    a++;
+}
+
+int fun2(int b){
+    if(b > 5){
+        return b;
+    }else{
+        return 0;
+    }
+
+}
+int main(){
+    fun1();
+    fun2(4);
+}
