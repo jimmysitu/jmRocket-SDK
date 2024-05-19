1a0618be66e35df63c321a9e9e982cf1d4f943c8
Jimmy Situ <web@jimmystone.cn>
2024-05-19 10:53:34 +0800

    Update lcov and gcov report script

--- baremetal/test_gcov/cov.sh
+++ baremetal/test_gcov/cov.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+rm *.gcda
 cat gcov-stream.bin | riscv64-unknown-elf-gcov-tool merge-stream
-riscv64-unknown-elf-gcov -bc test_gcov.c
-riscv64-unknown-elf-gcov -bc syscalls.c
+#riscv64-unknown-elf-gcov -bc test_gcov.c
+#riscv64-unknown-elf-gcov -bc syscalls.c
+
+lcov -d . -o test_gcov.info -b . -c \
+    --rc lcov_branch_coverage=1 \
+    --gcov-tool /opt/riscv/bin/riscv64-unknown-elf-gcov
+
+genhtml -rc genhtml_branch_coverage=1 test_gcov.info -o cov_result
+
+gcovr --txt-metric line --txt-metric branch --txt-metric decision \
+    --html-nested -r . \
+    --gcov-executable /opt/riscv/bin/riscv64-unknown-elf-gcov \
+    --gcov-ignore-errors no_working_dir_found \
+    --gcov-ignore-parse-error negative_hits.warn \
+    -o gcovr_result.html
--- baremetal/test_gcov/gdb.cmd
+++ baremetal/test_gcov/gdb.cmd
@@ -5,15 +5,13 @@ target remote localhost:3333
 load
 
 break main
-break fun1
-#break fun2
-break dump_gcov_info
+
+#break dump_gcov_info
 break tohost_exit
 break exit
 
-break fread
-break free
 
 continue
 
-dump binary memory gcov-stream.bin __gcda_start __gcda_end
+#dump binary memory gcov-stream.bin __gcda_start __gcda_end
+
